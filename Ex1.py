e1adeb3cc54552c8860245ed8a4b52cdd9e7e5ff
PappiPanda <104078442+PappiPanda@users.noreply.github.com>
2023-02-23 15:39:58 +0100

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,7 @@
+k1 = 'Hello'
+k2 = 'World'
+print(k1 + ' ' + k2)
+print ('Input name and age')
+n1 = input('Name: ')
+n2 = input('Age: ')
+print ('Your name ' + n1 + ' Your age ' + n2)
---
+++ Ex3.py
@@ -0,0 +1,8 @@
+n1 = input('Write any number: ')
+n2 = (n1 + n1)
+n3 = (n1 + n1 + n1)
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+print (' ')
+print (n1 + n2 + n3)
